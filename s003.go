8c678324c94df1655b79b967d46e153abc1e86dc
yangwenmai <yangwen.yw@gmail.com>
2020-09-22 20:29:47 +0800

    feat(s003): code and tests

--- s003.go
+++ s003.go
@@ -1,7 +1,7 @@
 package s003
 
 // 给定字符串 S1,S2, 判断字符串 S1 中的字符是否都在字符串 S2 中(S1 长度 N，S2长度 M)
-func hasContains(str, substr string) bool {
+func HasContains(str, substr string) bool {
 	slen1 := len(str)
 	slen2 := len(substr)
 	if slen2 > slen1 {
--- s003_test.go
+++ s003_test.go
@@ -8,14 +8,26 @@ import (
 
 func TestHasContains(t *testing.T) {
 	s1, s2 := "123", "12"
-	ret := hasContains(s1, s2)
+	ret := HasContains(s1, s2)
 	require.True(t, ret)
 
 	s1, s2 = "123", "1234"
-	ret = hasContains(s1, s2)
+	ret = HasContains(s1, s2)
 	require.False(t, ret)
 
 	s1, s2 = "123", "123"
-	ret = hasContains(s1, s2)
+	ret = HasContains(s1, s2)
 	require.True(t, ret)
+}
+
+// goos: darwin
+// goarch: amd64
+// BenchmarkHasContains-8          175624195                6.78 ns/op
+// PASS
+// ok      command-line-arguments  2.763s
+func BenchmarkHasContains(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		HasContains("1234", "12")
+	}
 }
